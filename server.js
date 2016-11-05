36ba1f5c9a4d467ce3b25ca9e7f2abeea605da70
Bill Hefty <billhefty@gmail.com>
2016-11-05 18:55:28 -0500

    add API to return ipaddress, langauge, and software

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+
+app.get('/api/whoami', function(req, res) {
+  let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+  let language = (req.headers['accept-language']).slice(0, 5)
+  let software = (req.headers['user-agent'])
+                  .match(/\(([^)]+)\)/)[1]
+
+  let whoami = {
+    ipaddress: ip,
+    language: language,
+    software: software
+  }
+
+  console.log(JSON.stringify(whoami, null, 2))
+
+  res.send(whoami)
+})
+
+app.listen(process.env.PORT || 8080, function(req, res) {
+  console.log("Server is listening.")
+})
